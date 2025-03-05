6bdc93c0963f23b3f7d32711b43ec9df8a729ea4
lytruongsinh123 <diepduong607@gmail.com>
2025-03-05 22:14:43 +0700

    Change

--- Algorithms/Sinh/Sinh2.cpp
+++ Algorithms/Sinh/Sinh2.cpp
@@ -5,19 +5,6 @@ using namespace std;
 int Bool[MAX] = {0};
 char res[MAX];
 
-<<<<<<< Tabnine <<<<<<<
-/**//+
- * @brief This function generates all permutations of a given string.//+
- *//+
- * The function uses a recursive approach to generate all permutations of a string.//+
- * It maintains a boolean array 'Bool' to keep track of characters that have already been used in the current permutation.//+
- * The 'res' array is used to store the current permutation.//+
- *//+
- * @param s The input string for which permutations need to be generated.//+
- * @param k The current position in the permutation.//+
- *//+
- * @return This function does not return a value. It prints all the permutations to the console.//+
- *///+
 void permutation(string s, int k) {
     if (k == s.size()) {
         cout << res << endl;
@@ -33,7 +20,7 @@ void permutation(string s, int k) {
         }
     }
 }
->>>>>>> Tabnine >>>>>>>// {"conversationId":"7fd22ce1-17fe-44a5-b727-e998bfb7561e","source":"instruct"}
+
 
 int main() {
     string s;
