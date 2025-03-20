4b677bf93d3ab3553abe97783cc7be882b477073
lytruongsinh123 <diepduong607@gmail.com>
2025-03-20 16:29:29 +0700

    change

---
+++ Practice/lecture8/ex2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void quick_sort(int start, int end) {
+
+}
+int main() {
+    int n; cin >> n;
+    vector<int> s;
+    for(int i = 0; i < n; i++) {
+        int x; cin >> x;
+        s.push_back(x);
+    }
+}
